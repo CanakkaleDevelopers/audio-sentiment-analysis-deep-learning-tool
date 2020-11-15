3c5abbbb0f7b8630b24876b5f4f52cbd0577b6e8
emirkivrak <emirccus@gmail.com>
2020-11-16 00:30:05 +0300

    dataset explorer added

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def main():
 
     # 1. Faz verisetlerinin meta çıkartımı TODO-> fonksiyon haline getir TODO-> daha çok veriseti ekle
     MetaDataCreator.ravdess_to_datatable()
-    #MetaDataCreator.cremad_to_datatable()
-    #MetaDataCreator.savee_to_datatable()
+    MetaDataCreator.cremad_to_datatable()
+    MetaDataCreator.savee_to_datatable()
+    MetaDataCreator.emodb_to_datatable()
 
     """
     1.5 Faz  -> 1. fazın yarattığı metadata_table.csv dosyasından kullanıcının isteklerine göre istenmeyen verileri sil,
@@ -16,7 +17,7 @@ def main():
     """
     2. Faz -> Öznitelikleri çıkart ve TEMP/FeaturesX.npy ve TEMP/FeaturesY.npy dosyaları oluşsun.
     """
-    FeatureExtractor.extract_from_metadata_table()
+    #FeatureExtractor.extract_from_metadata_table()
 
 
 
