b48f826865e0d8be51b8a68625220fab53ac17fd
Atakan Türkay <atakanturkay@stu.comu.edu.tr>
2020-11-23 05:27:27 +0300

    gui-0.03

--- flask-gui/app.py
+++ flask-gui/app.py
@@ -5,44 +5,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 from multiprocessing import Process
 
-
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from database.models import DatasetCatalog, DatasetMeta
+from database.models import DbDatasetCatalog, DbDatasetMeta, DbConfig
 
+#veritabanı modellerden bilgilerini alıp oluşturuluyor.
+db.metadata.create_all(db.engine)
 
-@app.route('/datacreator')
-def create_project():
-    # heavy_process = Process(
-    #     target=testmultithread.test_function,
-    #     daemon=True
-    # )
-    # heavy_process.start()
-    # email = User.query.filter(User.id == '1').first().email
-
-    # heavy_process = Process(
-    #     target= my_func,
-    #     daemon=True
-    # )
-    # heavy_process.start()
-    # testxd = DatasetMeta.query.all()
-
+@app.route('/')
+def page_welcome():
+    init_config()
     return render_template("index.html")
 
 
 @app.route('/test')
 def temp_func():
-
-    testxd = DatasetCatalog.query.first().children
+    testxd = DbDatasetCatalog.query.first().children
     for a in testxd:
         print(a.gender)
 
     return render_template("index.html")
-
 
 
 @app.teardown_appcontext
@@ -50,11 +35,81 @@ def shutdown_session(exception=None):
     db.session.remove()
 
 
-def my_func():
-    pass
+def init_config():
+    print("----------------")
+    print("Config işlemi başlatılıyor...")
+
+    if (DbConfig.query.all().__len__() == 0):
+        print("Config Bulunamadı , Oluşturuluyor.")
+        import os
+        import sys
+        import pathlib
+
+        working_dir_path = os.path.dirname(os.path.abspath(__file__))
+        if sys.platform.startswith('win32'):
+            TRAINING_FILES_PATH = str(working_dir_path) + '\\pass\\'
+            TRAINING_FILES_SPECTOGRAMS = str(working_dir_path) + '\\TEMP\\Spectogram\\'
+            SAVE_RUNTIME_FEATURES = str(working_dir_path) + '\\TEMP\\'
+            SAVE_RUNTIME_FEATURES_X = str(working_dir_path) + '\\TEMP\\featuresX.npy'
+            SAVE_RUNTIME_FEATURES_Y = str(working_dir_path) + '\\TEMP\\featuresY.npy'
+            MODEL_FEATURES_PATH = str(working_dir_path) + '\\ImplementedModels\\'
+            MODEL_WEIGHTS_PATH = str(working_dir_path) + '\\ModelWeights\\'
+            MODEL_TRAINING_PLOTS = str(working_dir_path) + '\\TEMP\\Plots'
+            TEST_FILES_PATH = str(working_dir_path) + '\\pass\\'
+            RAVDESS_FILES_PATH = str(working_dir_path) + '\\Datasets\\Ravdess'
+            CREMA_D_FILES_PATH = str(working_dir_path) + '\\Datasets\\Crema-D'
+            SAVEE_FILES_PATH = str(working_dir_path) + '\\Datasets\\SAVEE'
+            EMODB_FILES_PATH = str(working_dir_path) + '\\Datasets\\emoDB'
+            DATA_METADATA_DF_PATH = str(working_dir_path) + '\\TEMP\\datatable.csv'
+            db_config = DbConfig(TRAINING_FILES_PATH, TRAINING_FILES_SPECTOGRAMS, SAVE_RUNTIME_FEATURES,
+                                 SAVE_RUNTIME_FEATURES_X, SAVE_RUNTIME_FEATURES_Y, MODEL_FEATURES_PATH,
+                                 MODEL_WEIGHTS_PATH, MODEL_TRAINING_PLOTS, TEST_FILES_PATH, RAVDESS_FILES_PATH,
+                                 CREMA_D_FILES_PATH, SAVEE_FILES_PATH, EMODB_FILES_PATH, DATA_METADATA_DF_PATH)
+            db.session.add(db_config)
+            db.session.flush()
+
+        else:
+            TRAINING_FILES_PATH = str(working_dir_path) + '/pass/'
+            TRAINING_FILES_SPECTOGRAMS = str(working_dir_path) + '/TEMP/Spectogram'
+            MODEL_DIR_PATH = str(working_dir_path) + '/ImplementedModels/'
+            MODEL_WEIGHTS_PATH = str(working_dir_path) + '/ModelWeights'
+            MODEL_TRAINING_PLOTS = str(working_dir_path) + 'TEMP/Plots'
+            SAVE_RUNTIME_FEATURES = str(working_dir_path) + '/TEMP/'
+            SAVE_RUNTIME_FEATURES_X = str(working_dir_path) + '/TEMP/featuresX.npy'
+            SAVE_RUNTIME_FEATURES_Y = str(working_dir_path) + '/TEMP/featuresY.npy'
+            MODEL_FEATURES_PATH = str(working_dir_path) + '/ImplementedModels/'
+            TEST_FILES_PATH = str(working_dir_path) + '/pass/'
+            RAVDESS_FILES_PATH = str(working_dir_path) + '/Datasets/Ravdess'
+            CREMA_D_FILES_PATH = str(working_dir_path) + '/Datasets/Crema-D'
+            SAVEE_FILES_PATH = str(working_dir_path) + '/Datasets/SAVEE'
+            EMODB_FILES_PATH = str(working_dir_path) + '/Datasets/emoDB'
+            DATA_METADATA_DF_PATH = str(working_dir_path) + '/TEMP/metadata_table.csv'
+            db_config = DbConfig(TRAINING_FILES_PATH, TRAINING_FILES_SPECTOGRAMS, SAVE_RUNTIME_FEATURES,
+                                 SAVE_RUNTIME_FEATURES_X, SAVE_RUNTIME_FEATURES_Y, MODEL_FEATURES_PATH,
+                                 MODEL_WEIGHTS_PATH, MODEL_TRAINING_PLOTS, TEST_FILES_PATH, RAVDESS_FILES_PATH,
+                                 CREMA_D_FILES_PATH, SAVEE_FILES_PATH, EMODB_FILES_PATH, DATA_METADATA_DF_PATH)
+            db.session.add(db_config)
+            init_DbDatasetCatalog()
+            db.session.commit()
+
+    else:
+        print("Config Dosyası bulundu.")
+
+    print("Config İşlemi Tamamlandı.")
+
+    print("----------------")
 
 
-
+def init_DbDatasetCatalog():
+    print("----------------")
+    print("Catalog işlemi başlatılıyor...")
+    DbDatasetCatalog.query.delete()
+    db.session.add(DbDatasetCatalog("Crema-D",0,0))
+    db.session.add(DbDatasetCatalog("emoDB",0,0))
+    db.session.add(DbDatasetCatalog("Ravdess",0,0))
+    db.session.add(DbDatasetCatalog("SAVEE",0,0))
+    print("Catalog İşlemi Tamamlandı.")
+    print("----------------")
 
 
 if __name__ == '__main__':
--- flask-gui/database/models.py
+++ flask-gui/database/models.py
@@ -2,13 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from app import db
 
 
-class DatasetCatalog(db.Model):
+class DbDatasetCatalog(db.Model):
     __tablename__ = 'dataset_catalog'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     isdownloaded = db.Column(db.String(1))
     ismeta = db.Column(db.String(1))
-    children = db.relationship("DatasetMeta", backref="dataset_catalog" ,lazy=True)
+    children = db.relationship("DbDatasetMeta", backref="dataset_catalog" ,lazy=True)
 
     def __init__(self, name, isdownloaded, ismeta):
         self.name = name
@@ -19,7 +19,7 @@ class DatasetCatalog(db.Model):
         return '<DatasetCatalog %r>' % (self.id)
 
 
-class DatasetMeta(db.Model):
+class DbDatasetMeta(db.Model):
     __tablename__ = 'dataset_meta'
     id = db.Column(db.Integer, primary_key=True)
     dataset_catalog_id = db.Column(db.Integer, db.ForeignKey('dataset_catalog.id'))
@@ -32,6 +32,44 @@ class DatasetMeta(db.Model):
         self.gender = gender
         self.emotion = emotion
         self.path = path
+
+    def __repr__(self):
+        return '<DatasetMeta %r>' % (self.id)
+
+
+class DbConfig(db.Model):
+    __tablename__ = 'config'
+    id = db.Column(db.Integer, primary_key=True)
+    TRAINING_FILES_PATH = db.Column(db.String(500))
+    TRAINING_FILES_SPECTOGRAM = db.Column(db.String(500))
+    SAVE_RUNTIME_FEATURES = db.Column(db.String(500))
+    SAVE_RUNTIME_FEATURES_X = db.Column(db.String(500))
+    SAVE_RUNTIME_FEATURES_Y = db.Column(db.String(500))
+    MODEL_FEATURES_PATH = db.Column(db.String(500))
+    MODEL_WEIGHTS_PATH = db.Column(db.String(500))
+    MODEL_TRAINING_PLOTS = db.Column(db.String(500))
+    TEST_FILES_PATH = db.Column(db.String(500))
+    RAVDESS_FILES_PATH = db.Column(db.String(500))
+    CREMA_D_FILES_PATH = db.Column(db.String(500))
+    SAVEE_FILES_PATH = db.Column(db.String(500))
+    EMODB_FILES_PATH = db.Column(db.String(500))
+    DATA_METADATA_DF_PATH = db.Column(db.String(500))
+
+    def __init__(self, TRAINING_FILES_PATH, TRAINING_FILES_SPECTOGRAM, SAVE_RUNTIME_FEATURES,SAVE_RUNTIME_FEATURES_X,SAVE_RUNTIME_FEATURES_Y,MODEL_FEATURES_PATH,MODEL_WEIGHTS_PATH,MODEL_TRAINING_PLOTS,TEST_FILES_PATH,RAVDESS_FILES_PATH,CREMA_D_FILES_PATH,SAVEE_FILES_PATH,EMODB_FILES_PATH,DATA_METADATA_DF_PATH):
+        self.TRAINING_FILES_PATH = TRAINING_FILES_PATH
+        self.TRAINING_FILES_SPECTOGRAM = TRAINING_FILES_SPECTOGRAM
+        self.SAVE_RUNTIME_FEATURES = SAVE_RUNTIME_FEATURES
+        self.SAVE_RUNTIME_FEATURES_X = SAVE_RUNTIME_FEATURES_X
+        self.SAVE_RUNTIME_FEATURES_Y = SAVE_RUNTIME_FEATURES_Y
+        self.MODEL_FEATURES_PATH = MODEL_FEATURES_PATH
+        self.MODEL_WEIGHTS_PATH = MODEL_WEIGHTS_PATH
+        self.MODEL_TRAINING_PLOTS = MODEL_TRAINING_PLOTS
+        self.TEST_FILES_PATH = TEST_FILES_PATH
+        self.RAVDESS_FILES_PATH = RAVDESS_FILES_PATH
+        self.CREMA_D_FILES_PATH = CREMA_D_FILES_PATH
+        self.SAVEE_FILES_PATH = SAVEE_FILES_PATH
+        self.EMODB_FILES_PATH = EMODB_FILES_PATH
+        self.DATA_METADATA_DF_PATH = DATA_METADATA_DF_PATH
 
     def __repr__(self):
         return '<DatasetMeta %r>' % (self.id)
